441fc3cf6da2c083ea5ac3bccbdd35923b805ac8
Michal <michal.mencfel@gmail.com>
2023-11-07 22:04:29 +0100

    first model ready to use

--- main.py
+++ main.py
@@ -26,14 +26,14 @@ def __prepare_data():
 def __get_model(shape):
     custom_iou = CustomIoUMetric()
     model = models.Sequential()
-    model.add(layers.Conv2D(128, (3,3), activation = keras.activations.relu, input_shape = shape))
-    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(128, (12,12), activation = keras.activations.relu, input_shape = shape))
+    model.add(layers.MaxPooling2D((3, 3)))
     model.add(layers.Conv2D(32, (3, 3), activation = keras.activations.relu))
     model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(64, (3, 3), activation = keras.activations.relu))
+    # model.add(layers.Conv2D(64, (3, 3), activation = keras.activations.relu))
     model.add(layers.GlobalAveragePooling2D())
     model.add(layers.Dense(64, activation=keras.activations.relu))
-    model.add(layers.Dense(16, activation=keras.activations.selu))
+    # model.add(layers.Dense(16, activation=keras.activations.selu))
     model.add(layers.Dense(4, activation='linear'))
     model.compile(optimizer= keras.optimizers.Adam(), loss = custom_loss, metrics = [CustomIoUMetric()])
     model.summary()
@@ -42,10 +42,10 @@ def __get_model(shape):
 
 def train_network(train_data,train_labels,test_data,test_labels):
     network = __get_model(train_data[0].shape)
-    return network.fit(train_data,train_labels,batch_size = 2, validation_data=(test_data,test_labels), epochs=50)
+    return network.fit(train_data,train_labels,batch_size = 2, validation_data=(test_data,test_labels), epochs=25)
 
 
 if __name__ == "__main__":
     (train_images, train_boxes), (valid_images, valid_boxes) = __prepare_data()
-    # history = train_network(train_images,train_boxes,valid_images,valid_boxes)
-    # show_all_history(history)
+    history = train_network(train_images,train_boxes,valid_images,valid_boxes)
+    show_all_history(history)
